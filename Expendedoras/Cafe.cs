390f778a16cfbf607f44f467c67a88ab0fc3aa8d
mon-1001 <montsoleil25@gmail.com>
2024-08-21 12:01:44 -0600

    Add files via upload

---
+++ Expendedoras/Cafe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Expendedoras
+{
+    internal class Cafe : Expendedora
+    {
+        public Cafe()
+        {
+            Marca = "Nescafe";
+            Saludar();
+            Cleardisplay();
+            Console.WriteLine("Despierta con {0}", Marca);
+        }
+
+    }
+}
---
+++ Expendedoras/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Expendedoras
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Cafe miCCafe = new Cafe();
+            /*
+            Console.WriteLine("Entrar en modo mantenimiento? Si/No");
+            string mantenimiento = Console.ReadLine();
+
+            switch (mantenimiento)
+            {
+                case "Si":
+                    Console.Clear();
+                    Expendedora miexpendedora = new Expendedora(true);
+                    break;
+                case "No":
+                    Console.Clear();
+                    miexpendedora = new Expendedora();
+                    break; 
+            }
+            */
+            Console.ReadLine();
+               
+        }
+    }
+}
